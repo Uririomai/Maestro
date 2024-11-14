e139e0a950dd97431b2d3aa45649f472e2c69d76
Uririomai <Uririomai@yandex.ru>
2024-11-15 01:12:29 +0500

    add switch left and center logo in header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 import { ROUTES } from '../../utils/routes'
 
@@ -8,8 +8,22 @@ import styles from './header.module.scss'
 import LOGO from '../../assets/images/logo.png'
 
 const Header = () => {
+	const location = useLocation()
+	const [logoPositionInHeader, setLogoPosition] = useState(styles.header)
+
+	useEffect(() => {
+		switch (location.pathname) {
+			case '/sign-up':
+				setLogoPosition(styles.headerLeft)
+				break;
+			default:
+				setLogoPosition(styles.header)
+				break
+		}
+	}, [location.pathname])
+
 	return (
-		<header className={styles.header + ` container`}>
+		<header className={styles.header + ` ` + logoPositionInHeader + ` container` }>
 			<div className={styles.header__wrapper}>
 				<div className={styles.header__logo}>
 					<Link to={ROUTES.SIGNUP}>

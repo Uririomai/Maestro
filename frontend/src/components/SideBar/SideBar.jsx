ebcee64bb751888d8989ace6a2ac26391fec3b6f
Uririomai <Uririomai@yandex.ru>
2024-11-14 18:23:56 +0500

    add navigation active

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -2,33 +2,37 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 import styles from './sidebar.module.scss'
+import stylesNav from './nav.module.scss'
 
 const Sidebar = () => {
 	return (
 		<aside className={styles.sidebar}>
-			<nav className={`${styles.sidebar__nav} ${styles.nav}`}>
-				<ul className={styles.nav__list}>
-					<li className={styles.nav__item}>
-						<NavLink
-							to={`/cabinet`}
-							className={({ isActive }) =>
-								isActive ? 'nav__item--active' : ''
-							}
-						>
+			<nav className={`${styles.sidebar__nav} ${stylesNav.nav}`}>
+				<ul className={stylesNav.nav__list}>
+					<li className={stylesNav.nav__item}>
+						<NavLink className={stylesNav.nav__link} to={`/cabinet`}>
 							Кабинет
 						</NavLink>
 					</li>
-					<li className={styles.nav__item}>
-						<NavLink to={`/orders`}>Заказы</NavLink>
+					<li className={stylesNav.nav__item}>
+						<NavLink className={stylesNav.nav__link} to={`/orders`}>
+							Заказы
+						</NavLink>
 					</li>
-					<li className={styles.nav__item}>
-						<NavLink to={`/products`}>Товары</NavLink>
+					<li className={stylesNav.nav__item}>
+						<NavLink className={stylesNav.nav__link} to={`/products`}>
+							Товары
+						</NavLink>
 					</li>
-					<li className={styles.nav__item}>
-						<NavLink to={`/clients`}>Клиенты</NavLink>
+					<li className={stylesNav.nav__item}>
+						<NavLink className={stylesNav.nav__link} to={`/clients`}>
+							Клиенты
+						</NavLink>
 					</li>
-					<li className={styles.nav__item}>
-						<NavLink to={`/analytics`}>Аналитика</NavLink>
+					<li className={stylesNav.nav__item}>
+						<NavLink className={stylesNav.nav__link} to={`/analytics`}>
+							Аналитика
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
